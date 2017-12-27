997ae12f9d7865c0e358d9c0e32914ce0ec3fe1a
inductor <oota@hitomedia.jp>
2017-12-27 20:10:23 +0800

    refactor

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const web = new WebClient(token);
 
 // See: https://api.slack.com/methods/channels.list
 
-var fs = require('fs');
-var stream = fs.createWriteStream("channelList.md");
+const fs = require('fs');
+const stream = fs.createWriteStream("channelList.md");
 stream.once('open', function(fd) {
   stream.write("Name  | Topic | Persons\n");
   stream.write("----- | ----- | -------\n");
@@ -18,7 +18,8 @@ web.channels.list()
   .then((res) => {
     // `res` contains information about the channels
     for (let channel of res.channels) {
-      stream.write("[#" + channel.name + "](https://mohikanz.slack.com/messages/" + channel.name + ") |" + channel.topic.value + " | " + channel.num_members + "\n");
+        const escapedTopic = channel.topic.value.replace(/\r?\n/g, '');
+        stream.write(`[#${channel.name}](https://mohikanz.slack.com/messages/${channel.name}} | ${escapedTopic} | ${channel.num_members} \n`);
     }
     stream.end();
   })
